fdb868d281ded11164c12981d154f197c3aef546
Bahubali P Fuladi <bpfuladi@gmail.com>
2021-06-07 23:56:26 +0530

    Add files via upload

---
+++ I2C_Arduino/uC2uC_communication/i2C_Slave.ino
@@ -0,0 +1,63 @@
+/***************************************************
+   Arduino to Arduio communication using I2C
+   SLAVE Cade (this code will go in slave Arduino
+   Description: In this example slave code, slave will respond the master data request by sending data to master
+                and will read date send by master.
+                Slave name             Address
+                S8                      0x08
+                data received from master will be dislpayed on on-board-LED and 
+                when master request the data slave will send status of switch to master
+                
+                Note: 1. slave address could be any 8 bit value
+                      
+
+  Author : Bahubali Fuladi
+*/
+#include <Wire.h>
+int LED = 13;   //on board LED is connected on pin 13
+int sw = 12;    // switch is connected on pin 12
+int x = 0;
+void setup() {
+  // Define the LED pin as Output
+  Serial.begin(9600);
+  pinMode (LED, OUTPUT);
+  pinMode(sw, INPUT_PULLUP);
+  
+  // Start the I2C Bus as Slave on address 8
+  Wire.begin(8);  // this address must unique for slaves connected on same I2C bus
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  // Attach a function to trigger when request for data is reveived.
+  Wire.onRequest(requestEvent);
+}
+
+/********* An ISR to read the received data *************/
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+}
+void loop() {
+  //display the received data on LED
+  if (x == 0) {
+    digitalWrite(LED, HIGH);    //LED = ON if 0 is received
+  }
+  
+  if (x == 1) {
+    digitalWrite(LED, 0);   //LED = OFF if 1 is received
+  }
+}
+
+/********* An ISR to send data to master *************/
+void requestEvent() {
+  if (digitalRead(sw))  //reading status of switch 
+  {
+    Wire.write(0x04); // send 04 (some valve) to master for off switch
+    Serial.println("sw = OFF");
+  }
+  else
+  {
+    Wire.write(0x05); // send 05 (some other valve) to master for on switch
+    Serial.println("sw = ON");
+  }
+
+  // as expected by master
+}
---
+++ I2C_Arduino/uC2uC_communication/i2c_Master.ino
@@ -0,0 +1,55 @@
+/***************************************************
+ * Arduino to Arduio communication using I2C
+ * MASTER Cade (this code will go in master Arduino
+ * Description: In this example master code master will send(write) data to 2 slaves
+ *              and read date from same 2 slaves and display it on serial monitor.
+ *              Slave name             Address
+ *              S8                      0x08
+ *              S9                      0x09
+ *              Note: 1. slave address could be any 8 bit value 
+ *                    2. slave name is just a name used to display output it could be anything 
+ *                    
+ *Author : Bahubali Fuladi
+ */
+#include<Wire.h>
+
+int x = 0;
+void setup() {
+  // Start the I2C Bus as Master
+  Serial.begin(9600);
+  Wire.begin();     //initiate the Arduino I2C
+}
+
+void loop() {
+  /*************  this is to send data to slave device using there address ********/
+  Wire.beginTransmission(9); // transmit to device S9
+  Wire.write(x);              // sends x
+  Wire.endTransmission();    // stop transmitting
+  delay(500);
+  Wire.beginTransmission(8); // transmit to device S8
+  Wire.write(x);              // sends x
+  Wire.endTransmission();    // stop transmitting
+
+  x++; // x will toggle between 1 and 0
+  x = x & 0x01;
+  delay(500);
+  /************* this is to read data from slave devices using there address ********/
+  Wire.requestFrom(8, 1);    // request 1 bytes from slave device S8
+  while (Wire.available()) { // loop to read data coming from slave S8
+    char c = Wire.read(); // receive a byte as character
+    if (c == 0x04)        //reading the status of switch connected on slave S8 
+      Serial.print("S8 = OFF   ");         // print this if switch in OFF
+    if (c == 0x05)
+      Serial.print("S8 = ON    ");         // print this if switch in ON
+  }
+  Wire.requestFrom(9, 1);    // request 1 bytes from slave device S9
+
+  while (Wire.available()) { // loop to read data coming from slave S9
+    char c = Wire.read(); // receive a byte as character
+    if (c == 0x04)        //reading the status of switch connected on slave S9
+      Serial.print("S9 = OFF   ");         // print this if switch in OFF
+    if (c == 0x05)
+      Serial.print("S9 = ON    ");         // print this if switch in ON
+  }
+  Serial.println();
+}
